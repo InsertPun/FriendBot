ebc6a59d13b9ccc56b9e0c6a0b4dc67c533db20f
InsertPun <stanleyhuang905@gmail.com>
2025-03-01 23:11:24 -0500

    Testing deepseek:7b locally

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
-# Testing File
+import ollama
+response = ollama.chat(
+    model="deepseek-r1:7b",
+    messages=[
+        {"role": "user", "content": "Hello!"},
+    ],
+)
+print(response["message"]["content"])
